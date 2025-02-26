c61ef666b69f893dfb31fd3f60b1b60fe37a1321
SimonGebru <te.gebru@hotmail.se>
2025-02-26 20:37:20 +0100

    landing

--- airbean-group/src/components/Landing.jsx
+++ airbean-group/src/components/Landing.jsx
@@ -1,13 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom"; 
+import "./Landing.scss";
+import leftImage from "../assets/Left.png";
+import rightImage from "../assets/Right.png";
+import middleImage from "../assets/Middle.png";
 
-const landing = () => {
+const Landing = () => {
+  const navigate = useNavigate(); 
 
+  const handleClick = () => {
+    navigate("/nav"); 
+  };
 
   return (
-    <div className='landing'>
-      <h1>hej</h1>
-
+    <div className="landing" onClick={handleClick}>
+      <img src={leftImage} alt="Left leaves" className="leaves left" />
+      <img src={middleImage} alt="Air Bean logo" className="logo" />
+      <img src={rightImage} alt="Right leaves" className="leaves right" />
     </div>
-  )
-}
+  );
+};
 
-export default landing
+export default Landing;
