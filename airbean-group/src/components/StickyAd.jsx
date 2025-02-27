4d45a7dc1523e26e26951de458018eb6494bb48c
SimonGebru <te.gebru@hotmail.se>
2025-02-27 12:09:50 +0100

    stickyad

---
+++ airbean-group/src/components/StickyAd.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./StickyAd.scss"; // Skapa en separat SCSS-fil för styling
+
+const StickyAd = () => {
+  return (
+    <div className="sticky-ad">
+      <p><strong>KAMPANJ!</strong> <br />Bryggkaffe + Gustav Adolfsbakelse för endast 40 kr! 🎉</p>
+    </div>
+  );
+};
+
+export default StickyAd;
